5f76dbb7ffd5eabe759bbd600c1753f3a633a513
Hamza Umar <humar6078@gmail.com>
2022-04-20 11:53:04 +0000

    authenticate database

--- addUser.php
+++ addUser.php
@@ -1,4 +1,5 @@
  <?php 
+@include('config.php');
 
 // TODD:: add this section below to all authentication page
 // include('config.php');
@@ -7,58 +8,73 @@
 // }
 // TODD:: add this section above to all authentication page
   
+
+// Init success varialble
+$successMsg = "";
+$errorMsg = "";
+
+if (isset($_POST['addUser'])){
+  $staff_id=$_POST['staff_id'];
+    $role_id=$_POST['role_id'];
+
+    // Check if Staff Id exits
+    $staffQuery = $link->query("SELECT * FROM users WHERE staff_id = '$staff_id' ");
+    $staffCount = mysqli_num_rows($staffQuery);
+    if($staffCount != 0){
+      $errorMsg= "That Staff ID is already in used."; 
+    }else{
+
+      $insert = $link->query("INSERT INTO users (staff_id,role_id) VALUES($staff_id,$role_id)");
+      // assign success massage if inserted successfully
+      if($insert){
+        $successMsg = "User Has been added successfully.";
+        // header('Location: addUser.php');
+      }
+    }
+
+}
 include("header.php");
-@include('config.php');
   
 $que = mysqli_query($link,"select * from roles where 1");
 
-$successMsg = "";
-if (isset($_POST['addUser'])){
-  $staff_id=$_POST['staff_id'];
-  
-    $role_id=$_POST['role_id'];
-
-    $insert = $link->query("INSERT INTO users (staff_id,role_id) VALUES($staff_id,$role_id)");
-    if($insert){
-      $successMsg = "User Has been added successfully.";
-    }
-}
-
-?> 
+?>
  <div class="card ">
-   <div class="card-header">
-          <h3 class='text-center'>Add New User</h3>
-        </div>
-        <div class="cad-body">
-        <div style="width:600px; margin:0px auto">
-        <?php if ($successMsg){ ?>
-          <div class="alert alert-success" style="margin-top: 10px;"><?=$successMsg;?></div>
-        <?php } ?>
-            <form class="" action="" method="post">
-                <div class="form-group pt-3">
-                  <label for="name">Enter Staff ID</label>
-                  <input type="text" name="staff_id"  class="form-control" required>
-                </div>
-                
-                <div class="form-group">
-                  <div class="form-group">
-                    <label for="sel1">Select User Role</label> <br>
-                    <select class="form-control" name="role_id" id="role_id" required>
-                    <?php while($row = mysqli_fetch_assoc($que)){ ?>
-                        <option value="<?=$row['id'] ?>"><?=$row['role_name']; ?></option>;  
-               <?php }?>
-                    </select>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <button type="submit" name="addUser" class="btn btn-success">Add User</button>
-                </div>
+     <div class="card-header">
+         <h3 class='text-center'>Add New User</h3>
+     </div>
+     <div class="cad-body">
+         <div style="width:600px; margin:0px auto">
+             <!-- Show success message  -->
+             <?php if ($successMsg){ ?>
+             <div class="alert alert-success" style="margin-top: 10px;"><?=$successMsg;?></div>
+             <?php } ?>
+             <?php if ($errorMsg){ ?>
+             <div class="alert alert-danger" style="margin-top: 10px;"><?=$errorMsg;?></div>
+             <?php } ?>
+             <form class="" action="" method="post">
+                 <div class="form-group pt-3">
+                     <label for="name">Enter Staff ID</label>
+                     <input type="text" name="staff_id" class="form-control" required>
+                 </div>
+
+                 <div class="form-group">
+                     <div class="form-group">
+                         <label for="sel1">Select User Role</label> <br>
+                         <select class="form-control" name="role_id" id="role_id" required>
+                             <?php while($row = mysqli_fetch_assoc($que)){ ?>
+                             <option value="<?=$row['id'] ?>"><?=$row['role_name']; ?></option>;
+                             <?php }?>
+                         </select>
+                     </div>
+                 </div>
+                 <div class="form-group">
+                     <button type="submit" name="addUser" class="btn btn-success">Add User</button>
+                 </div>
 
 
-            </form>
-          </div>
-        </div>
-      </div>
+             </form>
+         </div>
+     </div>
+ </div>
 
  <?php include("footer.php"); ?>
-
--- header.php
+++ header.php
@@ -1,82 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="application-name" content="Milestone" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="Milestone" />
-        <meta name="theme-color" content="#4C7FF0" />
-        <title>Milestone - Bootstrap 4 Dashboard Template</title>
-        <script src="../../cdn-cgi/apps/head/OD5ndB08NwL1oUgIYmaZ8k2oR2M.js"></script>
-        <link rel="stylesheet" href="vendor/bower-jvectormap/jquery-jvectormap-1.2.2.css"><link/>
-        <link rel="stylesheet" href="styles/app.min.css" />
-        <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
-      rel="stylesheet">
-       <link rel="stylesheet" href="styles/auth-form.css" />
-   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-    </head>
-    <body>
-        <div class="app">
-            <div class="off-canvas-overlay" data-toggle="sidebar"></div>
-            <?php include("menu.php"); ?>
-            <div class="main-panel">
-                <nav class="header navbar">
-                    <div class="header-inner">
-                        <div class="navbar-item navbar-spacer-right brand hidden-lg-up">
-                            <a href="javascript:;" data-toggle="sidebar" class="toggle-offscreen"><i class="material-icons">menu</i> </a> <a class="brand-logo hidden-xs-down"><img src="images/fc-logo.png" width="100" height="300" alt="logo" /> </a>
-                        </div>
-                        <a class="navbar-item navbar-spacer-right navbar-heading hidden-md-down" href="dashboard.php"><span>Dashboard</span></a>
-                        <div class="navbar-item nav navbar-nav">
-                            <div class="nav-item nav-link dropdown">
-                                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"><i class="material-icons">notifications</i> <span class="tag tag-danger">4</span></a>
-                                <div class="dropdown-menu dropdown-menu-right notifications">
-                                    <div class="dropdown-item">
-                                        <div class="notifications-wrapper">
-                                            <ul class="notifications-list">
-                                                <li>
-                                                    <a href="javascript:;">
-                                                        <div class="notification-icon">
-                                                            <div class="circle-icon bg-success text-white"><i class="material-icons">arrow_upward</i></div>
-                                                        </div>
-                                                        <div class="notification-message"><b>Sean</b> launched a new application <span class="time">2 seconds ago</span></div>
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="javascript:;">
-                                                        <div class="notification-icon">
-                                                            <div class="circle-icon bg-danger text-white"><i class="material-icons">check</i></div>
-                                                        </div>
-                                                        <div class="notification-message"><b>Removed calendar</b> from app list <span class="time">4 hours ago</span></div>
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="javascript:;">
-                                                        <span class="notification-icon"><span class="circle-icon bg-info text-white">J</span></span>
-                                                        <div class="notification-message"><b>Jack Hunt</b> has <b>joined</b> mailing list <span class="time">9 days ago</span></div>
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="javascript:;">
-                                                        <span class="notification-icon"><span class="circle-icon bg-primary text-white">C</span></span>
-                                                        <div class="notification-message"><b>Conan Johns</b> created a new list <span class="time">9 days ago</span></div>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div class="notification-footer">Notifications</div>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1" />
+    <meta name="msapplication-tap-highlight" content="no" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="application-name" content="Milestone" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="Milestone" />
+    <meta name="theme-color" content="#4C7FF0" />
+    <title>Milestone - Bootstrap 4 Dashboard Template</title>
+    <script src="../../cdn-cgi/apps/head/OD5ndB08NwL1oUgIYmaZ8k2oR2M.js"></script>
+    <link rel="stylesheet" href="vendor/bower-jvectormap/jquery-jvectormap-1.2.2.css">
+    <link />
+    <link rel="stylesheet" href="styles/app.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="styles/auth-form.css" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body>
+    <div class="app">
+        <div class="off-canvas-overlay" data-toggle="sidebar"></div>
+        <?php include("menu.php"); ?>
+        <div class="main-panel">
+            <nav class="header navbar">
+                <div class="header-inner">
+                    <div class="navbar-item navbar-spacer-right brand hidden-lg-up">
+                        <a href="javascript:;" data-toggle="sidebar" class="toggle-offscreen"><i
+                                class="material-icons">menu</i> </a> <a class="brand-logo hidden-xs-down"><img
+                                src="images/fc-logo.png" width="100" height="300" alt="logo" /> </a>
+                    </div>
+                    <a class="navbar-item navbar-spacer-right navbar-heading hidden-md-down"
+                        href="dashboard.php"><span>Dashboard</span></a>
+                    <div class="navbar-item nav navbar-nav">
+                        <div class="nav-item nav-link dropdown">
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"><i
+                                    class="material-icons">notifications</i> <span class="tag tag-danger">4</span></a>
+                            <div class="dropdown-menu dropdown-menu-right notifications">
+                                <div class="dropdown-item">
+                                    <div class="notifications-wrapper">
+                                        <ul class="notifications-list">
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <div class="notification-icon">
+                                                        <div class="circle-icon bg-success text-white"><i
+                                                                class="material-icons">arrow_upward</i></div>
+                                                    </div>
+                                                    <div class="notification-message"><b>Sean</b> launched a new
+                                                        application <span class="time">2 seconds ago</span></div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <div class="notification-icon">
+                                                        <div class="circle-icon bg-danger text-white"><i
+                                                                class="material-icons">check</i></div>
+                                                    </div>
+                                                    <div class="notification-message"><b>Removed calendar</b> from app
+                                                        list <span class="time">4 hours ago</span></div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <span class="notification-icon"><span
+                                                            class="circle-icon bg-info text-white">J</span></span>
+                                                    <div class="notification-message"><b>Jack Hunt</b> has <b>joined</b>
+                                                        mailing list <span class="time">9 days ago</span></div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <span class="notification-icon"><span
+                                                            class="circle-icon bg-primary text-white">C</span></span>
+                                                    <div class="notification-message"><b>Conan Johns</b> created a new
+                                                        list <span class="time">9 days ago</span></div>
+                                                </a>
+                                            </li>
+                                        </ul>
                                     </div>
+                                    <div class="notification-footer">Notifications</div>
                                 </div>
                             </div>
-                           
                         </div>
+
                     </div>
-                </nav>
+                </div>
+            </nav>
